d2aa1140ae27a4351bc8c480df6e4dacab9e0368
Srujan Bharadwaj <srujanbharadwaj44@gmail.com>
2021-03-07 16:41:28 +0530

    Add few more models

--- rent_app/admin.py
+++ rent_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import House, HouseImages, User, Owner
+from .models import User, Owner, House, HouseImages, Review, House_Management
 
 
 admin.site.register(User)
+admin.site.register(Owner)
 admin.site.register(House)
 admin.site.register(HouseImages)
-admin.site.register(Owner)
+admin.site.register(Review)
+admin.site.register(House_Management)
--- rent_app/models.py
+++ rent_app/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 
+RATING_CHOICES = (
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+    ("6", "6"),
+    ("7", "7"),
+    ("8", "8"),
+)
+
+
 class User(models.Model):
 
     # null  = True -> This attribute in the database can be NULL
@@ -43,7 +55,7 @@ class House(models.Model):
     rent            = models.IntegerField()
     owner_id        = models.ForeignKey(Owner,on_delete=models.CASCADE)
     description     = models.TextField(null=True)
-    vacant          = models.BooleanField
+    vacant          = models.BooleanField(default=False)
 
     def __str__(self):
         return self.address
@@ -55,4 +67,21 @@ class HouseImages(models.Model):
     id              = models.AutoField(primary_key=True)
 
 
-# class Review(models.Model):
+class Review(models.Model):
+    # Review id will be auto generated.
+    user_id         = models.ForeignKey(User, on_delete=models.CASCADE)
+    house_id        = models.ForeignKey(House, on_delete=models.CASCADE)
+    description     = models.CharField(max_length=100)
+    rating          = models.CharField(max_length=20, choices=RATING_CHOICES, null=False)
+    # A single user can give review only once => The tuple (user_id, house_id) is unique in Review table
+    class Meta:
+        unique_together = (
+            ('user_id', 'house_id'),
+        )
+
+
+class House_Management(models.Model):
+    user_id         = models.ForeignKey(User, on_delete=models.CASCADE)
+    house_id        = models.ForeignKey(House, on_delete=models.CASCADE)
+    owner_id        = models.ForeignKey(Owner,on_delete=models.CASCADE)
+    date_time       = models.DateTimeField(auto_now_add=True)
