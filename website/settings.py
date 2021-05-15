042400bfdcc0ff4c41803e555a79fd9204e48be0
Srujan Bharadwaj <srujanbharadwaj44@gmail.com>
2021-05-15 16:20:09 +0530

    Security fix

--- website/settings.py
+++ website/settings.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'projectdbms2021@gmail.com'
-EMAIL_HOST_PASSWORD = 'srujarya28'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
